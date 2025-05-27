4d6c8949207d02070b5520e4cfbb4a13338ca633
TenzinJhopee <tenzin.jeunesse@gmail.com>
2025-05-27 15:25:03 +0200

    ex011 implement pair ou impair

---
+++ ex011/pw_pair_impair.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+char pw_pair_impair(char *str) {
+      if (str == NULL) {
+        return 'N';
+    }
+    int count = 0;
+    char *current = str;
+    while (*current != '\0') {
+        if ((*current >= 'A' && *current <= 'Z') || (*current >= 'a' && *current <= 'z')) {
+            count++;
+        }
+        current++;
+    }
+    return (count % 2 == 0) ? 'P' : 'I';
+}
